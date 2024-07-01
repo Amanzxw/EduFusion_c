d0d0522f075298763e8e2e104819271aee3f1c4f
Amandeep kaur <amankaur77702@gmail.com>
2024-07-01 12:12:21 +0530

    Updated .gitignore and modified Logout.js and AuthProvider.js

--- src/Components/Logout.js
+++ src/Components/Logout.js
@@ -1,38 +1,4 @@
-// import React from 'react';
-// import { useAuth } from '../context/AuthProvider';
 
-// const logout = () => {
-//   const { logout } = useAuth(); // Assuming useAuth provides a logout function
-
-//   const handleLogout = () => {
-//     logout(); // Call logout function provided by AuthProvider
-//   };
-
-//   return (
-//     <div>
-//       <button
-//         style={{
-//           backgroundColor: 'red',
-//           color: 'white',
-//           border: 'none',
-//           padding: '10px 20px',
-//           textAlign: 'center',
-//           textDecoration: 'none',
-//           display: 'inline-block',
-//           fontSize: '16px',
-//           margin: '4px 2px',
-//           cursor: 'pointer',
-//           borderRadius: '4px',
-//         }}
-//         onClick={handleLogout}
-//       >
-//         Logout
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default logout;
 
 import React from 'react';
 import { useAuth } from '../context/AuthProvider';
